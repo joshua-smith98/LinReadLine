f748d0b47fb38455a66d1248c13e5ce45e1a66ca
joshua-smith98 <jrichard.smith98@gmail.com>
2024-04-18 09:09:12 +1000

    Added history test to Testing project

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,7 +1,9 @@
 ﻿using LinuxConsoleReadLineFix;
 
+while(true)
+{
 Console.Write(">> ");
 var result = LinuxTerminalFix.ReadLine();
-Console.WriteLine();
 Console.WriteLine(result);
-Console.ReadKey(true);
+Console.WriteLine();
+}
